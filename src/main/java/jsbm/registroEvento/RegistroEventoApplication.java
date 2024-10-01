e394b82031135b71c01862c55e8c148f5084ba41
JSebastianBe <sebastianbm97@gmail.com>
2024-10-01 13:02:11 -0500

    Subida inicial

---
+++ src/main/java/jsbm/registroEvento/RegistroEventoApplication.java
@@ -0,0 +1,13 @@
+package jsbm.registroEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jsbm/registroEvento/RegistroEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package jsbm.registroEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
